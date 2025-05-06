7962843dc57e4c4633a673c33073d25651cbb774
Lovely Tiwari <lovelyt7989@gmail.com>
2025-05-07 00:15:39 +0530

    Putting whole project

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <p>Copyright &copy; Community Project by Lovely, Nancy and Utakrsh 2025 May.</p>
+        </div>
+    )
+}
